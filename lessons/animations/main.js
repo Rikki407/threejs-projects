760861dfa0d2326ea90e38c03ed330c75b08c695
Rishab Lamba <rishablamba407@gmail.com>
2021-06-13 13:21:58 +0530

    UPDATE: Animation lesson completed

--- lessons/animations/main.js
+++ lessons/animations/main.js
@@ -1,20 +1,18 @@
 import './style.css';
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 /* ---------------------------------- Scene --------------------------------- */
 const scene = new THREE.Scene();
 
 /* --------------------------------- Object --------------------------------- */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff1298 });
+const material = new THREE.MeshBasicMaterial({ color: 0xa909a4 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 /* ---------------------------------- Sizes --------------------------------- */
-const sizes = {
-    width: 800,
-    height: 600,
-};
+const sizes = { width: 800, height: 600 };
 
 /* --------------------------------- Camera --------------------------------- */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
@@ -26,4 +24,24 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.getElementById('webgl-canvas'),
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+/* ------------------------------- Animations ------------------------------- */
+
+// Clock
+// const clock = new THREE.Clock();
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2 , x: 0 });
+
+const tick = () => {
+    // const elapsedTime = clock.getElapsedTime();
+
+    // Update Object
+    // mesh.rotation.x = elapsedTime * Math.PI;
+    // mesh.position.y = Math.sin(elapsedTime);
+
+    // Render
+    
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+};
+tick();
--- main.js
+++ main.js
@@ -1,3 +1,2 @@
 import './style.css';
 
-document.querySelector('#app').innerHTML = `<h1>Hello World</h1>`;
